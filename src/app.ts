043a26239b53dced8bbefbd6646ef21106ec95b1
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-02-08 09:01:25 +0530

    catalog service setup done

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Hello from catalog service!" });
+});
+
+app.use(globalErrorHandler);
+
+export default app;
