bff362c9f71a43657f4131380ff150157dc360da
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-30 10:20:00 +0530

    add broker factory to create the broker with only one instance called singleton

--- src/common/factories/brokerFactory.ts
+++ src/common/factories/brokerFactory.ts
@@ -1,6 +1,6 @@
 // create a singleton of broker in order to avoid multiple broker instances
 
-import { KafkaProducerBroker } from "../../config/kafka";
+import { KafkaProducerBroker } from "../../services/kafka";
 import { MessageProducerBroker } from "../constants/brokerType";
 import config from "config";
 
